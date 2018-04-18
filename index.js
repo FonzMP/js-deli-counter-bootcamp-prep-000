58767d6dc306bde71ac5464f9e4b5f55979690e0
Alex Wilson <fonzmp@gmail.com>
2018-04-18 00:13:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ function insertPerson(katzDeliLine, name){
   let katDeliLine = katzDeli.push(name);
   for (let i = 0; i < katzDeliLine.length; i++){
     currentPerson = katzDeliLine[i];
-    console.log(currentPerson);
     currentPosition = parseInt(i) + 1;
-    console.log(currentPosition);
   }
   return katzDeliLine;
 }
