7347952f3d0ffee5c92f9e49d882ba0ad18ec435
Alex Wilson <fonzmp@gmail.com>
2018-04-18 00:07:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name){
   let katDeliLine = katzDeli.push(name);
   for (let i = 0; i < katzDeliLine.length; i++){
-    return name, i + 1;
+    return katzDeli[i];
   }
 }
