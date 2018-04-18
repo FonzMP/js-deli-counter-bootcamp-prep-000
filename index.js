9188b48c65297a0e00b89ccc336878f60bfbbc2f
Alex Wilson <fonzmp@gmail.com>
2018-04-18 00:46:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(){
   let firstPerson = katzDeli.shift();
-  if (firstPerson) {
+  if (firstPerson === false) {
     return "There is nobody waiting to be served!"
   }
 }
