2675bb21019335268512c1889aa0aa641c6ad77e
Alex Wilson <fonzmp@gmail.com>
2018-04-18 00:42:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var katzDeli = [];
 
-function insertPerson(katzDeliLine, name){
-  let katDeliLine = katzDeli.push(name);
-  for (let i = 0; i < katzDeliLine.length; i++){
-    currentPerson = katzDeliLine[i];
-    currentPosition = parseInt(i) + 1;
-  }
-  return katzDeliLine;
-}
+// function insertPerson(katzDeliLine, name){
+//   let katDeliLine = katzDeli.push(name);
+//   for (let i = 0; i < katzDeliLine.length; i++){
+//     currentPerson = katzDeliLine[i];
+//     currentPosition = parseInt(i) + 1;
+//   }
+//   return katzDeliLine;
+// }
 
-function takeANumberExample(katzDeliLine, name) {
-  let position = "";
-  let currentSearchName = String(name);
-  for (let i = 0; i < katzDeliLine.length; i++) {
-    let currentUser = String(katzDeliLine[i]);
-    if (currentUser === currentSearchName) {
-      return "Welcome, " + currentUser + ". You are number " + parseInt(i + 1) + " in line.";
-    } else {
-      position = "Sorry, we couldn't find that name."
-    }
-  }
-  return position
-}
+// function takeANumberExample(katzDeliLine, name) {
+//   let position = "";
+//   let currentSearchName = String(name);
+//   for (let i = 0; i < katzDeliLine.length; i++) {
+//     let currentUser = String(katzDeliLine[i]);
+//     if (currentUser === currentSearchName) {
+//       return "Welcome, " + currentUser + ". You are number " + parseInt(i + 1) + " in line.";
+//     } else {
+//       position = "Sorry, we couldn't find that name."
+//     }
+//   }
+//   return position
+// }
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
