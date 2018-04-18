40148174de7c6ea27bf0292dc87241405a811fce
Alex Wilson <fonzmp@gmail.com>
2018-04-18 00:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function takeANumber(katzDeliLine, name) {
   let currentSearchName = name;
   for (let i = 0; i < katzDeliLine.length; i++) {
     let currentUser = katzDeliLine[i];
+    console.log(currentUser)
     if (currentUser === currentSearchName) {
       position = parseInt(i) + 1;
     } else {
